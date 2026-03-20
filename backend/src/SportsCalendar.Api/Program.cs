4245b52a33df344e50f11d8fe1eced6b88c7655f
skmkqw <tkorsakov77@gmail.com>
2026-03-20 23:51:07 +0100

    config: create and apply infrastructure DI layer

--- backend/src/SportsCalendar.Api/Program.cs
+++ backend/src/SportsCalendar.Api/Program.cs
@@ -1,8 +1,9 @@
+using SportsCalendar.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
-
-app.UseHttpsRedirection();
 
 app.Run();
