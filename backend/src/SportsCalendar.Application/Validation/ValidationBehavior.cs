360e3c22e7690cee4568c96e480e1786c34ce261
skmkqw <tkorsakov77@gmail.com>
2026-03-21 00:21:19 +0100

    feat: create validation mediatR pipeline behavior

---
+++ backend/src/SportsCalendar.Application/Validation/ValidationBehavior.cs
@@ -0,0 +1,38 @@
+using ErrorOr;
+using FluentValidation;
+using MediatR;
+
+namespace SportsCalendar.Application.Validation;
+
+public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
+    where TResponse : IErrorOr
+{
+    private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+    public ValidationBehavior(IEnumerable<IValidator<TRequest>> validators)
+    {
+        _validators = validators;
+    }
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        if (!_validators.Any())
+            return await next();
+
+        var context = new ValidationContext<TRequest>(request);
+        var validationResults = await Task.WhenAll(_validators.Select(v => v.ValidateAsync(context, cancellationToken)));
+        var failures = validationResults.SelectMany(result => result.Errors).Where(f => f != null).ToList();
+
+        if (failures.Count != 0)
+        {
+            var errors = failures
+                .Select(e => Error.Validation(e.PropertyName, e.ErrorMessage))
+                .ToList();
+
+            return (dynamic)errors;
+        }
+
+        return await next();
+    }
+}
