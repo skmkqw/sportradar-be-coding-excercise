3bf79b1b8b4675a051ddf03e50f33d2defcb66ab
skmkqw <tkorsakov77@gmail.com>
2026-03-21 22:48:28 +0100

    feat: implement applicatiob healthchecks (api + database)

--- backend/src/SportsCalendar.Api/Program.cs
+++ backend/src/SportsCalendar.Api/Program.cs
@@ -1,3 +1,5 @@
+using System.Text.Json;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using SportsCalendar.Api;
 using SportsCalendar.Api.Middlewares;
 using SportsCalendar.Application;
@@ -15,6 +17,25 @@ builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
 builder.Services.AddProblemDetails();
 
 var app = builder.Build();
+
+app.MapHealthChecks("/health", new HealthCheckOptions
+{
+    ResponseWriter = async (context, report) =>
+    {
+        context.Response.ContentType = "application/json";
+        var response = new
+        {
+            Status = report.Status.ToString(),
+            Checks = report.Entries.Select(e => new
+            {
+                Component = e.Key,
+                Status = e.Value.Status.ToString(),
+                e.Value.Description
+            })
+        };
+        await context.Response.WriteAsync(JsonSerializer.Serialize(response));
+    }
+});
 
 app.UseExceptionHandler();
 app.MapControllers();
--- backend/src/SportsCalendar.Infrastructure/DependencyInjection.cs
+++ backend/src/SportsCalendar.Infrastructure/DependencyInjection.cs
@@ -2,6 +2,7 @@ using System.Data;
 using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using SportsCalendar.Application.Interfaces.Repositories;
 using SportsCalendar.Infrastructure.Repositories.Events;
 
@@ -15,7 +16,18 @@ public static class DependencyInjection
 
         services.AddScoped<IDbConnection>(_ => new SqlConnection(connectionString));
 
+        // Repositories
         services.AddScoped<IEventRepository, EventRepository>();
+
+        // Healthcheks
+        services
+            .AddHealthChecks()
+            .AddCheck("self", () => HealthCheckResult.Healthy("API is responsive"))
+            .AddSqlServer(
+                connectionString: connectionString!,
+                name: "sqlserver",
+                tags: ["db", "sql"]
+            );
 
         return services;
     }
