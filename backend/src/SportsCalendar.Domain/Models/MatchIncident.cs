d2c809565b43e4c46463eb26c1aa28dd03d6955d
skmkqw <tkorsakov77@gmail.com>
2026-03-23 05:53:27 +0100

    fix: add missing references in domain layer

--- backend/src/SportsCalendar.Domain/Models/MatchIncident.cs
+++ backend/src/SportsCalendar.Domain/Models/MatchIncident.cs
@@ -12,28 +12,34 @@ public class MatchIncident
 
     public int MatchMinute { get; private set; }
 
+    public Guid ResultId { get; init; }
+
     public DateTime CreatedAtUtc { get; init; }
 
     private MatchIncident() { }
 
-    private MatchIncident(Guid id, Guid teamId, IncidentType type, int matchMinute)
+    private MatchIncident(Guid id, Guid teamId, IncidentType type, int matchMinute, Guid resultId)
     {
         Id = id;
         TeamId = teamId;
         Type = type;
         MatchMinute = matchMinute;
+        ResultId = resultId;
 
         CreatedAtUtc = DateTime.UtcNow;
     }
 
-    public static MatchIncident Create(Guid teamId, IncidentType type, int matchMinute)
+    public static MatchIncident Create(Guid teamId, IncidentType type, int matchMinute, Guid resultId)
     {
         if (teamId == Guid.Empty)
             throw new ArgumentException("Team id can't be empty.");
 
+        if (resultId == Guid.Empty)
+            throw new ArgumentException("Result id can't be empty.");
+
         if (matchMinute < 1)
             throw new ArgumentException("Match minute must be greater than 0.");
 
-        return new MatchIncident(Guid.NewGuid(), teamId, type, matchMinute);
+        return new MatchIncident(Guid.NewGuid(), teamId, type, matchMinute, resultId);
     }
 }
--- backend/src/SportsCalendar.Domain/Models/PeriodScore.cs
+++ backend/src/SportsCalendar.Domain/Models/PeriodScore.cs
@@ -10,21 +10,24 @@ public class PeriodScore
 
     public int AwayScore { get; private set; }
 
+    public Guid ResultId { get; init; }
+
     public DateTime CreatedAtUtc { get; init; }
 
     private PeriodScore() { }
 
-    private PeriodScore(Guid id, int periodNumber, int homeScore, int awayScore)
+    private PeriodScore(Guid id, int periodNumber, int homeScore, int awayScore, Guid resultId)
     {
         Id = id;
         PeriodNumber = periodNumber;
         HomeScore = homeScore;
         AwayScore = awayScore;
+        ResultId = resultId;
 
         CreatedAtUtc = DateTime.UtcNow;
     }
 
-    public static PeriodScore Create(int periodNumber, int homeScore, int awayScore)
+    public static PeriodScore Create(int periodNumber, int homeScore, int awayScore, Guid resultId)
     {
         if (periodNumber < 0)
             throw new ArgumentException("Period number can't be less than 0.");
@@ -32,10 +35,12 @@ public class PeriodScore
         if (homeScore < 0)
             throw new ArgumentException("Home score can't be less than 0.");
 
-
         if (awayScore < 0)
             throw new ArgumentException("Away score can't be less than 0.");
 
-        return new PeriodScore(Guid.NewGuid(), periodNumber, homeScore, awayScore);
+        if (resultId == Guid.Empty)
+            throw new ArgumentException("Result id can't be empty.");
+
+        return new PeriodScore(Guid.NewGuid(), periodNumber, homeScore, awayScore, resultId);
     }
 }
